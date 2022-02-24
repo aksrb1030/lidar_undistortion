3646795552c06088f1d13f6104fe0b73fcf4c182
mgkim <aksrb10@gmail.com>
2022-02-24 12:23:45 +0900

    sync_20

--- include/undistortion.hpp
+++ include/undistortion.hpp
@@ -1,22 +1,44 @@
 #include <iostream>
+#include <queue>
+#include <iterator>
+#include <future>
+#include <chrono>
+
 #include <ros/ros.h>
-#include <pcl_conversions/pcl_conversions.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <sensor_msgs/NavSatFix.h>
+#include <sensor_msgs/Imu.h>
 #include <visualization_msgs/Marker.h>
 #include <visualization_msgs/MarkerArray.h>
 #include <nav_msgs/Odometry.h>
 #include <nav_msgs/Path.h>
 #include <tf/tf.h>
 #include <tf/transform_broadcaster.h>
+
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_types.h>
 #include <Eigen/Core>
-#include <sensor_msgs/Imu.h>
-#include <queue>
-#include <iterator>
-#include <future>
-#include <chrono>
+
+struct PointXYZIT {
+  PCL_ADD_POINT4D   
+  float intensity;
+  double timestamp;
+  uint16_t ring;                   
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW  
+} EIGEN_ALIGN16;                   
+
+POINT_CLOUD_REGISTER_POINT_STRUCT(
+    PointXYZIT,
+    (float, x, x)(float, y, y)(float, z, z)(float, intensity, intensity)(
+        double, timestamp, timestamp)(uint16_t, ring, ring))
+
+typedef PointXYZIT PointType;
+typedef pcl::PointCloud<PointType>::Ptr PointTypeCloud;
+
 
 int num = 0;
+
+int scanIdx_ = 0;
 
 bool newfullCloud_ = false;
 
@@ -26,12 +48,19 @@ using Eigen4x1d = Eigen::Matrix<double, 4, 1>;
 
 
 
+double lidarTimeOffset_ = 0.3;
 
 pcl::PointCloud<pcl::PointXYZI>::Ptr point_cloud_;
-pcl::PointCloud<pcl::PointXYZI>::Ptr laserCloudFullRes_;
+PointTypeCloud laserCloudFullRes_;
+
+PointTypeCloud currCloud_;
+PointTypeCloud lastCloud_;
+
+sensor_msgs::ImuConstPtr lastImuMsg_;
 
 std::mutex mutexLidarQueue_;
 std::queue<sensor_msgs::PointCloud2ConstPtr> lidarMsgQueue_;
+
 std::mutex mutexIMUQueue_;
 std::queue<sensor_msgs::ImuConstPtr> imuMsgQueue_;
 
@@ -42,6 +71,8 @@ ros::Publisher l2I_cloud_pub;
 double scan_time_;
 
 
+double lidar_curr_t_;
+double lidar_last_t_;
 
 double scan_period_{0.1};
 static const int imu_que_length_{200};
@@ -75,4 +106,4 @@ void imuCallBack(const sensor_msgs::ImuConstPtr &msg);
 void AccumulateIMUShiftAndRotation();
 void process();
 void distortionCloud();
-bool imuTimeSync(double startTime, double endTime, std::vector<sensor_msgs::ImuConstPtr> &vimuMsg);
+bool imuTimeSync(double startTime, double endTime, std::vector<sensor_msgs::ImuConstPtr> &vimuMsg, sensor_msgs::ImuConstPtr &lastImuMsg);
--- src/undistortion.cpp
+++ src/undistortion.cpp
@@ -7,30 +7,34 @@ void pointCallBack(const sensor_msgs::PointCloud2ConstPtr &msg)
     Eigen::Vector3d t = Eigen::Vector3d::Zero();
     Eigen3x4d Rt;
 
-    pcl::PointCloud<pcl::PointXYZI>::Ptr point_cloud(new pcl::PointCloud<pcl::PointXYZI>());
-    point_cloud_->clear();
-    pcl::fromROSMsg(*msg, *point_cloud);
-    scan_time_ = msg->header.stamp.toSec();
+    // PointTypeCloud point_cloud(new pcl::PointCloud<PointType>());
 
-    for (auto &p:*point_cloud)
-    {
-        pcl::PointXYZI pt;
+    // pcl::PointCloud<PointType>::Ptr point_cloud = pcl::PointCloud<PointType>::Ptr(new pcl::PointCloud<PointType>);
 
-        // Eigen::MatrixXd lidar_points ;
-        // lidar_points << p.x , p.y, p.z, 1;
+    // point_cloud_->clear();
+    // pcl::fromROSMsg(*msg, *point_cloud);
 
-        Eigen4x1d lidar_points = Eigen4x1d::Identity();
-        lidar_points << p.x , p.y, p.z, 1;
+    // scan_time_ = msg->header.stamp.toSec();
+    // pcl::PointCloud<pcl::PointXYZI>::Ptr l2I_points_cloud(new pcl::PointCloud<pcl::PointXYZI>());
+    // for (auto &p:*point_cloud)
+    // {
+    //     pcl::PointXYZI pt;
 
-        Eigen::MatrixXd l2I = L2I_tm_ * lidar_points;
+    //     // Eigen::MatrixXd lidar_points ;
+    //     // lidar_points << p.x , p.y, p.z, 1;
 
-        pt.x = l2I(0);
-        pt.y = l2I(1);
-        pt.z = l2I(2);
-        pt.intensity = p.intensity;
+    //     Eigen4x1d lidar_points = Eigen4x1d::Identity();
+    //     lidar_points << p.x , p.y, p.z, 1;
 
-        point_cloud_->push_back(pt);
-    }
+    //     Eigen::MatrixXd l2I = L2I_tm_ * lidar_points;
+
+    //     pt.x = l2I(0);
+    //     pt.y = l2I(1);
+    //     pt.z = l2I(2);
+    //     pt.intensity = p.intensity;
+
+    //     l2I_points_cloud->push_back(pt);
+    // }
 
     // sensor_msgs::PointCloud2 l2I_cloud_msg;
     // sensor_msgs::PointCloud2ConstPtr l2I_const_cloud_msg;
@@ -42,8 +46,8 @@ void pointCallBack(const sensor_msgs::PointCloud2ConstPtr &msg)
     // l2I_const_cloud_msg->
 
     // push lidar msg to queue
-    // std::unique_lock<std::mutex> lock(mutexLidarQueue_);
-    // lidarMsgQueue_.push(msg);
+    std::unique_lock<std::mutex> lock(mutexLidarQueue_);
+    lidarMsgQueue_.push(msg);
 }
 
 void AccumulateIMUShiftAndRotation()
@@ -216,19 +220,20 @@ void distortionCloud()
             }
             point_cloud_->points[i] = p;
             imu_ptr_last_iter_ = imu_ptr_front_;
-
         }
-            sensor_msgs::PointCloud2 l2I_cloud_msg;
-            sensor_msgs::PointCloud2ConstPtr l2I_const_cloud_msg;
-            pcl::toROSMsg(*point_cloud_, l2I_cloud_msg);
-            l2I_cloud_msg.header.frame_id = "map";
-            l2I_cloud_msg.header.stamp = ros::Time::now();
-            l2I_cloud_pub.publish(l2I_cloud_msg);
-
+        sensor_msgs::PointCloud2 l2I_cloud_msg;
+        sensor_msgs::PointCloud2ConstPtr l2I_const_cloud_msg;
+        pcl::toROSMsg(*point_cloud_, l2I_cloud_msg);
+        l2I_cloud_msg.header.frame_id = "map";
+        l2I_cloud_msg.header.stamp = ros::Time::now();
+        l2I_cloud_pub.publish(l2I_cloud_msg);
     }
 }
 void imuCallBack(const sensor_msgs::ImuConstPtr &imuIn)
 {
+    std::unique_lock<std::mutex> lock(mutexIMUQueue_);
+    imuMsgQueue_.push(imuIn);
+
     double roll, pitch, yaw;
     tf::Quaternion orientation;
 
@@ -314,10 +319,10 @@ void imuCallBack(const sensor_msgs::ImuConstPtr &imuIn)
     //     imu_angular_rot_z_[imu_ptr_last_] = imu_angular_rot_z_[imu_ptr_back] + angular_velo(2) * time_diff;
     // }
 
-    AccumulateIMUShiftAndRotation();
+    // AccumulateIMUShiftAndRotation();
 }
 
-bool imuTimeSync(double startTime, double endTime, std::vector<sensor_msgs::ImuConstPtr> &vimuMsg)
+bool imuTimeSync(double startTime, double endTime, std::vector<sensor_msgs::ImuConstPtr> &vimuMsg, sensor_msgs::ImuConstPtr &lastImuMsg)
 {
 
     /** \brief 특정시간(LiDAR와의 sync)대의 IMU 데이터를 가져오는 함수
@@ -328,9 +333,21 @@ bool imuTimeSync(double startTime, double endTime, std::vector<sensor_msgs::ImuC
 
     std::unique_lock<std::mutex> lock(mutexIMUQueue_);
     double current_time = 0;
+
     vimuMsg.clear();
+
+    bool lastImuFlg = false;
+    
+    if(lastImuMsg_->header.stamp.toSec() != 0)
+    {
+        std::cout <<lastImuMsg_->header.stamp.toSec() << "\n";
+        vimuMsg.push_back(lastImuMsg);
+    }
+
     while (true)
     {
+        
+
         if (imuMsgQueue_.empty())
         {
             break;
@@ -345,9 +362,19 @@ bool imuTimeSync(double startTime, double endTime, std::vector<sensor_msgs::ImuC
 
         sensor_msgs::ImuConstPtr &tmpimumsg = imuMsgQueue_.front();
         double time = tmpimumsg->header.stamp.toSec();
-        if (time <= endTime && time > startTime)
+        if (time <= endTime && time >= startTime)
         {
+            // std::cout << std::fixed<<">>time : " << time << "\n";
+            // std::cout << std::fixed<<">>endTime : " << endTime << "\n";
+            // std::cout << std::fixed<<">>startTime : " << startTime << "\n";
+            // std::cout << " ================" << "\n";
+            // exit(1);
+            
+             
+            
             vimuMsg.push_back(tmpimumsg);
+            // qimuMsg_.push(tmpimumsg);
+            
             current_time = time;
             imuMsgQueue_.pop();
 
@@ -355,99 +382,152 @@ bool imuTimeSync(double startTime, double endTime, std::vector<sensor_msgs::ImuC
             if (time == endTime)
                 break;
         }
-        else
-        {
-            std::cout << "else check "
-                      << "\n";
+        
+
+        // else
+        // {
+        //     std::cout << "else check "
+        //               << "\n";
+        //               exit(1);
             if (time <= startTime)
             {
                 imuMsgQueue_.pop();
             }
-            else
-            {
-                double dt_1 = endTime - current_time;
-                double dt_2 = time - endTime;
+        //     else
+        //     {
+        //         double dt_1 = endTime - current_time;
+        //         double dt_2 = time - endTime;
 
-                ROS_ASSERT(dt_1 >= 0);
-                ROS_ASSERT(dt_2 >= 0);
-                ROS_ASSERT(dt_1 + dt_2 > 0);
-                double w1 = dt_2 / (dt_1 + dt_2);
-                double w2 = dt_1 / (dt_1 + dt_2);
-                sensor_msgs::ImuPtr theLastIMU(new sensor_msgs::Imu);
-                theLastIMU->linear_acceleration.x = w1 * vimuMsg.back()->linear_acceleration.x + w2 * tmpimumsg->linear_acceleration.x;
-                theLastIMU->linear_acceleration.y = w1 * vimuMsg.back()->linear_acceleration.y + w2 * tmpimumsg->linear_acceleration.y;
-                theLastIMU->linear_acceleration.z = w1 * vimuMsg.back()->linear_acceleration.z + w2 * tmpimumsg->linear_acceleration.z;
-                theLastIMU->angular_velocity.x = w1 * vimuMsg.back()->angular_velocity.x + w2 * tmpimumsg->angular_velocity.x;
-                theLastIMU->angular_velocity.y = w1 * vimuMsg.back()->angular_velocity.y + w2 * tmpimumsg->angular_velocity.y;
-                theLastIMU->angular_velocity.z = w1 * vimuMsg.back()->angular_velocity.z + w2 * tmpimumsg->angular_velocity.z;
-                theLastIMU->header.stamp.fromSec(endTime);
-                vimuMsg.emplace_back(theLastIMU);
-                break;
-            }
-        }
+        //         ROS_ASSERT(dt_1 >= 0);
+        //         ROS_ASSERT(dt_2 >= 0);
+        //         ROS_ASSERT(dt_1 + dt_2 > 0);
+        //         double w1 = dt_2 / (dt_1 + dt_2);
+        //         double w2 = dt_1 / (dt_1 + dt_2);
+        //         sensor_msgs::ImuPtr theLastIMU(new sensor_msgs::Imu);
+        //         theLastIMU->linear_acceleration.x = w1 * vimuMsg.back()->linear_acceleration.x + w2 * tmpimumsg->linear_acceleration.x;
+        //         theLastIMU->linear_acceleration.y = w1 * vimuMsg.back()->linear_acceleration.y + w2 * tmpimumsg->linear_acceleration.y;
+        //         theLastIMU->linear_acceleration.z = w1 * vimuMsg.back()->linear_acceleration.z + w2 * tmpimumsg->linear_acceleration.z;
+        //         theLastIMU->angular_velocity.x = w1 * vimuMsg.back()->angular_velocity.x + w2 * tmpimumsg->angular_velocity.x;
+        //         theLastIMU->angular_velocity.y = w1 * vimuMsg.back()->angular_velocity.y + w2 * tmpimumsg->angular_velocity.y;
+        //         theLastIMU->angular_velocity.z = w1 * vimuMsg.back()->angular_velocity.z + w2 * tmpimumsg->angular_velocity.z;
+        //         theLastIMU->header.stamp.fromSec(endTime);
+        //         vimuMsg.emplace_back(theLastIMU);
+        //         break;
+        //     }
+        // }
     }
     return !vimuMsg.empty();
 }
 
 void process()
 {
+
     double time_last_lidar = -1;
     double time_curr_lidar = -1;
+
     std::vector<sensor_msgs::ImuConstPtr> vimuMsg;
+    // std::queue<sensor_msgs::ImuConstPtr> qimuMsg;
+
+    sensor_msgs::ImuConstPtr lastImuMsg;
 
     while (ros::ok())
     {
-        laserCloudFullRes_.reset(new pcl::PointCloud<pcl::PointXYZI>);
+        currCloud_.reset(new pcl::PointCloud<PointType>);
+        // lastCloud_.reset(new pcl::PointCloud<PointType>);
+
         std::unique_lock<std::mutex> lock_lidar(mutexLidarQueue_);
 
         if (!lidarMsgQueue_.empty())
         {
             // get new lidar msg
             time_curr_lidar = lidarMsgQueue_.front()->header.stamp.toSec();
-            // std::cout << " lidarMsgQueue_.front()->header.stamp.toSec() : " << lidarMsgQueue_.front()->header.stamp.toSec() << "\n";
+            pcl::fromROSMsg(*lidarMsgQueue_.front(), *currCloud_);
 
-            pcl::fromROSMsg(*lidarMsgQueue_.front(), *laserCloudFullRes_);
             lidarMsgQueue_.pop();
             newfullCloud_ = true;
+            scanIdx_++;
+            // std::cout << scanIdx_ << "\n";
+
         }
+        else
+        {
+            newfullCloud_ = false;
+        }
+        
         lock_lidar.unlock();
 
         if (newfullCloud_)
         {
-            nav_msgs::Odometry debugInfo;
-            debugInfo.pose.pose.position.x = 0;
-            debugInfo.pose.pose.position.y = 0;
-            debugInfo.pose.pose.position.z = 0;
-
-            if (time_last_lidar > 0)
+            if (scanIdx_ >= 3)
             {
-                // get IMU msg int the Specified time interval
-                vimuMsg.clear();
-                int countFail = 0;
-
-                while (!imuTimeSync(time_last_lidar, time_curr_lidar, vimuMsg))
+                // std::cout << scanIdx_ << "\n";
+                if (time_last_lidar > 0)
                 {
-                    countFail++;
-                    if (countFail > 100)
+                    // vimuMsg.clear();
+                    int countFail = 0;
+                    // std::cout << "in" << "\n";
+                    while (!imuTimeSync(time_last_lidar, time_curr_lidar, vimuMsg, lastImuMsg))
                     {
-                        break;
+
+                        countFail++;
+                        if (countFail > 100)
+                        {
+                            break;
+                        }
+                        // std::this_thread::sleep_for(std::chrono::milliseconds(10));
                     }
-                    // std::this_thread::sleep_for(std::chrono::milliseconds(10));
+                }
+
+                if (!vimuMsg.empty())
+                {
+                    
+
+                    std::cout << " cur size : " << currCloud_->size() << "\n";
+                    std::cout << "lidar size : " << lastCloud_->points.size() << "\n";
+                    
+                    std::cout << std::fixed << "lidar [0] :" << lastCloud_->points[0].timestamp << "\n";
+
+                    std::cout << "vimuMsg.size() : " << vimuMsg.size() << "\n";
+
+
+                    std::cout << "vimmsg [1] : " << vimuMsg[1]->header.stamp.toSec() << "\n";
+                    std::cout << "vimmsg [E] : " << vimuMsg[vimuMsg.size() - 1]->header.stamp.toSec() << "\n";
+                    // std::cout << "vimmsg [0] : " << vimuMsg[0]->header.stamp.toSec() << "\n";
+                    
+
+                    std::cout << "lidar [E] :" << lastCloud_->points[lastCloud_->size() - 1].timestamp << "\n";
+                    std::cout << "\n";
+
+                    // std::cout << lastImuMsg->header.stamp.toSec() << "\n";
+                    std::cout << "================"
+                              << "\n";
+
+                    lastImuMsg = vimuMsg[vimuMsg.size()-1];
+
+                }
+
+                time_last_lidar = time_curr_lidar;
+
+                // std::cout << "1" <<"\n";
+                // std::cout << lastImuMsg->header.stamp.toSec() << "\n";
+                // std::cout << "vimusize : " << vimuMsg[vimuMsg.size()-1] << "\n";
+                
+
+                
+                lastCloud_->clear();
+                
+                for (auto &p : currCloud_->points)
+                {
+                    PointType points;
+                    points.x = p.x;
+                    points.y = p.y;
+                    points.z = p.z;
+                    points.intensity = p.intensity;
+                    points.timestamp = p.timestamp;
+                    lastCloud_->push_back(points);
                 }
             }
-
-            if (!vimuMsg.empty())
-            {
-                std::cout << "vimuMsg.size() : " << vimuMsg.size() << "\n";
-                std::cout << "================"
-                          << "\n";
-            }
-            // else
-            // {
-            //     std::cout << "vimuMsg.empty() !" << "\n";
-            // }
-
-            time_last_lidar = time_curr_lidar;
+            // lastCloud_->insert(currCloud_);
         }
     }
 }
@@ -474,32 +554,40 @@ int main(int argc, char **argv)
 
     Eigen::Translation3d L2I_tl(vecL2I[3], vecL2I[4], vecL2I[5]);
 
-    laserCloudFullRes_.reset(new pcl::PointCloud<pcl::PointXYZI>);
+    currCloud_.reset(new pcl::PointCloud<PointType>);
+    lastCloud_.reset(new pcl::PointCloud<PointType>);
+
     point_cloud_.reset(new pcl::PointCloud<pcl::PointXYZI>);
+    currCloud_.reset(new pcl::PointCloud<PointType>);
+
+    // lastCloud_ = PointTypeCloud(new pcl::PointCloud<PointType>);
+
+    // pcl::PointCloud<PointType>::Ptr(new pcl::PointCloud<PointType>);
 
     L2I_tm_ = (L2I_tl * L2I_z_rot * L2I_y_rot * L2I_x_rot).matrix().matrix().block(0, 0, 3, 4);
     // ros::Subscriber subFullCloud = nodeHandler.subscribe<sensor_msgs::PointCloud2>(point_cloud_topic, 10, pointCallBack);
 
+    ros::Subscriber sub_imu = nodeHandler.subscribe(imu_topic, 2000, imuCallBack);
     ros::Subscriber subFullCloud = nodeHandler.subscribe(point_cloud_topic, 10, pointCallBack);
     // ros::Subscriber sub_imu = nodeHandler.subscribe(imu_topic, 2000, imuCallBack)
 
     // ros::TransportHints() -> net work 통신 방법 지정해줌
     // unrealiable : 신뢰할 수 없는 전송을 지정. UDP라는데 정확한 설명 X
-    ros::Subscriber sub_imu = nodeHandler.subscribe(imu_topic, 2000, imuCallBack, ros::TransportHints().unreliable());
 
     l2I_cloud_pub = nodeHandler.advertise<sensor_msgs::PointCloud2>("l2I_cloud_topic", 1, true);
 
-    ros::Rate rate(200);
-    while (ros::ok())
-    {
-        ros::spinOnce();
+    // ros::Rate rate(200);
+    // while (ros::ok())
+    // {
 
-        distortionCloud();
+    //     ros::spinOnce();
 
-        rate.sleep();
-    }
+    //     distortionCloud();
 
-    // std::thread thread_process{process};
+    //     rate.sleep();
+    // }
+
+    std::thread thread_process{process};
 
     ros::spin();
 
